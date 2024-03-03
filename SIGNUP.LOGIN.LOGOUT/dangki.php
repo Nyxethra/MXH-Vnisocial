72ce0b7792f193e3868b946b67968225c43a6965
Qkhanhs <2254810158@vaa.edu.vn>
2024-03-03 10:44:12 +0700

    sua lai dang ki

--- SIGNUP.LOGIN.LOGOUT/dangki.php
+++ SIGNUP.LOGIN.LOGOUT/dangki.php
@@ -1,36 +1,41 @@
 <?php
+session_start();
 
-class Signup
-{
-    private $error = "";
-    public function evaluate($data)
-    {
-        foreach($data as $key => $value){
-            if(empty($value))
-            {
-                $this->error = $this->error . $key . " dang de trong! <br>";
-            }
+// Kết nối đến cơ sở dữ liệu MySQL
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "vnisocial";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Kiểm tra kết nối
+if ($conn->connect_error) {
+    die("Kết nối tới cơ sở dữ liệu thất bại: " . $conn->connect_error);
+}
+
+// Kiểm tra xem người dùng đã gửi biểu mẫu đăng ký chưa
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Lấy giá trị từ biểu mẫu đăng ký
+    $email = $_POST['email'];
+    $matkhau = $_POST['matkhau'];
+
+    // Truy vấn cơ sở dữ liệu để kiểm tra xem tên người dùng đã tồn tại hay chưa
+    $checkQuery = "SELECT * FROM nguoidung WHERE email = '$email'";
+    $checkResult = $conn->query($checkQuery);
+
+    if ($checkResult->num_rows > 0) {
+        // Tên người dùng đã tồn tại
+        echo 'Tên đăng nhập đã tồn tại!';
+    } else {
+        // Thực hiện truy vấn để thêm người dùng mới vào cơ sở dữ liệu
+        $insertQuery = "INSERT INTO nguoidung (email, matkhau) VALUES ('$email', '$matkhau')";
+        if ($conn->query($insertQuery) === TRUE) {
+            // Đăng ký thành công
+            header("location:../TRANG_CANHAN/trangcanhan.php");
+        } else {
+            // Đăng ký không thành công
+            echo 'Đăng ký không thành công. Vui lòng thử lại!';
         }
-        if($this->error =="")
-        {
-            //no error
-            $this->create_user($data);
-        }else
-        {
-            return $this->error;
-        }
-    }
-    public function create_user($data)
-    {
-        $name=$data['name'];
-        $matkhau =$data['matkhau'];
-        $email =$data['email'];
-
-        $query = "insert into nguoidung( ten_nguoidung , matkhau, email)
-        values ('$name', '$matkhau', '$email')";
-
-        $DB = new Database();
-        $DB->save($query);
     }
 }
-?>
