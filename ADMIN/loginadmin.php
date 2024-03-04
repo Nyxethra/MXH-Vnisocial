3d693542f7e65cec5a5c3f64edc2e51aea3f5c2f
Quang <gnauq1210@gmail.com>
2024-03-04 10:00:18 +0700

    admin

---
+++ ADMIN/loginadmin.php
@@ -0,0 +1,40 @@
+
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "admin";
+session_start();
+
+// Tạo kết nối
+$db = new mysqli($servername, $username, $password, $dbname);
+
+// Kiểm tra kết nối
+if ($db->connect_error) {
+  die("Kết nối thất bại: " . $db->connect_error);
+}
+
+// Lấy dữ liệu từ form đăng nhập
+$email = $_POST['email'];
+$matkhau = $_POST['matkhau'];
+
+// Kiểm tra thông tin đăng nhập
+$query = "SELECT * FROM inforadmin WHERE email = '$email'";
+$result = $db->query($query);
+$user = $result->fetch_assoc();
+
+if ($matkhau == $user['matkhau']) {
+    // Nếu thông tin đăng nhập chính xác, thiết lập phiên
+    $_SESSION['email'] = $email;
+
+    // Nếu người dùng là quản trị viên, chuyển hướng họ đến trang index của quản trị viên
+    if ($email == 'quang') {
+        $_SESSION['isAdmin'] = true;
+        header('Location: index.php');
+        exit();
+    }
+} else {
+    // Nếu thông tin đăng nhập không chính xác, hiển thị lỗi
+    echo "Thông tin đăng nhập không chính xác.";
+}
+?>
