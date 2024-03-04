a0bf04ee4a0357bef6d227b6679bc4a8c4f0e73f
Quang <gnauq1210@gmail.com>
2024-03-05 02:11:25 +0700

    .

--- ADMIN/index.php
+++ ADMIN/index.php
@@ -6,7 +6,7 @@
     <title>ADMIN | VNISOCIAL</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
     <style>
-        body {
+        body {  
             font-family: Arial, sans-serif;
             margin: 0;
             padding: 0;
