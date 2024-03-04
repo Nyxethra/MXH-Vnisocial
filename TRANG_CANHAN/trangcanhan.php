dd5da01c8f46a34b6ec406352b3dd217551baeca
Qkhanhs <2254810158@vaa.edu.vn>
2024-03-04 17:13:35 +0700

    sua loi trangcanhan

--- TRANG_CANHAN/trangcanhan.php
+++ TRANG_CANHAN/trangcanhan.php
@@ -10,6 +10,7 @@
     <!-- Thanh công cụ -->
     <?php include ('../BAR/Nav_Bar.php');?>
         <!-- phần giao diện chính -->
+        <div style="display: flex">
         <div class="main_interface"  >
                 <div style="width:100%">
                     <img src="pic.jpg"  class="anhbia">
@@ -73,6 +74,7 @@
                 </div>
                 </div>
         </div>
+    
         <!-- Phần bạn bè -->
         <div class="friend_bar">
             Bạn bè<br>
@@ -107,5 +109,6 @@
             </div>
         </div>
     </div>
+</div>
 </body>
 </html>
