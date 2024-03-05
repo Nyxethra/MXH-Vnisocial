101026b5c18e2dd9efd2fa38421a3d5e34521231
BaoNgocnetroi <2254810194@vaa.edu.vn>
2024-03-05 17:21:16 +0700

    .

--- SIGNUP.LOGIN.LOGOUT/dangxuat.php
+++ SIGNUP.LOGIN.LOGOUT/dangxuat.php
@@ -1,14 +1,13 @@
 <?php
-// Bắt đầu phiên làm việc
 session_start();
 
-// Hủy bỏ tất cả các session
-session_unset();
+// Hủy bỏ tất cả các biến phiên làm việc
+$_SESSION = array();
 
 // Hủy bỏ phiên làm việc
 session_destroy();
 
-// Chuyển hướng người dùng đến trang chủ hoặc trang đăng nhập
-header("Location: login.php"); // Thay thế 'index.php' bằng trang mà bạn muốn chuyển hướng sau khi đăng xuất
-exit;
+// Chuyển hướng người dùng về trang đăng nhập sau khi đăng xuất
+header('Location: login.php');
+exit();
 ?>
