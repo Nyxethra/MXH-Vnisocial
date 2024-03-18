a6c8be5a2e12a12ab6f31ede67913af25de78395
Qkhanhs <2254810158@vaa.edu.vn>
2024-03-18 21:09:16 +0700

    test

--- TRANG_CANHAN/text_html.php
+++ TRANG_CANHAN/text_html.php
@@ -50,16 +50,33 @@ session_start();
     }
     ?>
     <div class="profile-picture">
-        <img id="avatar-img" class="profile_pic">
+    <?php if (isset($_SESSION['avatar'])): ?>
+        <img id="avatar-img" src="../img/gallery/?php echo $_SESSION['avatar']; ?>" class="profile_pic">
+    <?php else: ?>
+        <img id="avatar-img" src="../img/gallery/default_avatar.png" class="profile_pic">
+    <?php endif; ?>
         <form action="" method="POST" enctype="multipart/form-data">
             <input type="file" name="avatar" id="avatar-input" accept="image/*" style="display:none">
             <button type="button" id="edit-avatar-btn" class="edit-avatar">
                 <i class="fas fa-camera"></i>
             </button>
-            <input type="submit" value="Lưu" >
+            <input type="submit" value="Lưu" id='show' style="display:none">
         </form>
     </div>
-    
+    <script>
+        document.getElementById('edit-avatar-btn').addEventListener('click', function() {
+            document.getElementById('avatar-input').click();
+        });
+
+        document.getElementById('avatar-input').addEventListener('change', function(e) {
+            var reader = new FileReader();
+            reader.onload = function(event) {
+                document.getElementById('show').style.display = 'block';
+                document.getElementById('avatar-img').src = event.target.result;
+            };
+            reader.readAsDataURL(e.target.files[0]);
+        });
+    </script>
 
 </body>
 </html>
--- TRANG_CANHAN/trangcanhan.php
+++ TRANG_CANHAN/trangcanhan.php
@@ -25,7 +25,7 @@
                     
                     <img src="../img/pic.jpg" class="anhbia">
                     <div class="head__user" style="display:flex">
-                    <div style="width: 803px; text-align:center ; background-color: white; position: absolute; bottom: 0;    ">
+                    <div style="width: 654px;right:171px; text-align:center ; position: absolute; bottom: 153px;    ">
                         <div class="menu_buttons">Dòng thời gian </div>
                         <div class="menu_buttons">Giới thiệu </div>
                         <div class="menu_buttons">Bạn bè</div>
