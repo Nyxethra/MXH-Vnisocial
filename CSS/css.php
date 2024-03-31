30018c5a7924a7ff675cc1519a160c92a11f334e
Thecoffeemint <new.horizons1001101@yahoo.com>
2024-04-01 00:41:05 +0700

    .

---
+++ CSS/css.php
@@ -0,0 +1,12 @@
+<style>
+    .custom-post {
+    margin-top: 30px;
+    margin-bottom: 30px;
+    border: 1px solid #ddd;
+    border-radius: 8px;
+    padding: 20px;
+    background: white;
+    width: 110%;
+    margin-left: 18px;
+}
+</style>
