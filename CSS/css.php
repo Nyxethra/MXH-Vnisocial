4f1a34c8977650c5e2995cf9c3e115845657eb30
Thecoffeemint <new.horizons1001101@yahoo.com>
2024-04-01 02:03:10 +0700

    .

--- CSS/css.php
+++ CSS/css.php
@@ -1,12 +1,13 @@
 <style>
-    .custom-post {
+.custom-post {
     margin-top: 30px;
-    margin-bottom: 30px;
+    /* margin-bottom: 30px; */
     border: 1px solid #ddd;
     border-radius: 8px;
     padding: 20px;
     background: white;
     width: 110%;
-    margin-left: 18px;
+    margin-left: 19px;
 }
+
 </style>
